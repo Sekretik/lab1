403e67dfb6407fe3ecc55b714e97fe356de58b66
Sekretik <90332361+Sekretik@users.noreply.github.com>
2022-11-14 01:01:24 +0500

    2

--- lessons/lab_1/lab.py
+++ lessons/lab_1/lab.py
@@ -2,113 +2,99 @@ import pandas as pd
 import numpy as np
 import math, os
 
-visiting_table = pd.read_excel('lessons\\lab_1\\Успеваемость групп.xlsx',sheet_name='Посещение', header=None,skiprows=3)
-homeworck_table = pd.read_excel('lessons\\lab_1\\Успеваемость групп.xlsx',sheet_name='ДЗ', header=None,skiprows=3)
+visiting_table = pd.read_excel('lessons\\lab_1\\Успеваемость групп.xlsx',sheet_name='Посещение', header=None)
+homework_table = pd.read_excel('lessons\\lab_1\\Успеваемость групп.xlsx',sheet_name='ДЗ', header=None,skiprows=3)
 test_table = pd.read_excel('lessons\\lab_1\\Успеваемость групп.xlsx',sheet_name='КТ', header=None,skiprows=2)
+
+columns = visiting_table.loc[[0,2]].T
+columns = columns.ffill(limit=1)
+
+visiting_table.columns = pd.MultiIndex.from_frame(columns)
+visiting_table = visiting_table.drop(index=[0, 1, 2])
+visiting_table = visiting_table.dropna(how='all', axis=1)
+
+visiting_table = visiting_table.set_index(visiting_table[(np.nan, 'ID')].squeeze().values)
+visiting_table = visiting_table.drop(columns=(np.nan, 'ID'))
+visiting_table.index.name = 'ID'
 
 path = 'lessons\\lab_1\\ДЗ\\'
 
 dir =  os.listdir(path=path)
 
+hw_table = pd.DataFrame()
+
 for file in dir:
     ids_dict = {}
-    number = int(file[0])
+    number = file[0]
 
-    hw_table = pd.read_excel(path+file)
-    for i in range(len(hw_table)):
-        ids_dict[hw_table.iloc[i,7]] = round(hw_table.iloc[i,6])
+    hw = pd.read_excel(path+file)
 
-    for i in range(len(homeworck_table)):
-        student = homeworck_table.iloc[i,0]
-        if(student in ids_dict):
-            homeworck_table.loc[i,number] = ids_dict[student]
+    hw_table[0]  = hw['ID']
+    hw_table[number]  = hw['Процент правильных ответов (%)']
 
-def getVisitingByStudent(index):
-    vis = 0
-    vis_count = 0
-    for i in range(1,len(visiting_table.iloc[index]),2):
-        count = visiting_table.iloc[index,i]
-        if(not math.isnan(count)):
-            vis += count
-            vis_count += 1
+homework_table = homework_table.replace(r'^\s*$', np.nan, regex=True).dropna(axis=0,how='all').dropna(axis=1,how='all')
+homework_table = homework_table.merge(hw_table,how='left',on=0).replace(np.nan, 0, regex=True)
+homework_table = round(homework_table)
+
+homework_table = homework_table.set_index(homework_table[0].squeeze().values)
+homework_table = homework_table.drop(columns=(0))
+homework_table.index.name = 'ID'
+
+test_table = test_table.set_index(test_table[0].squeeze().values)
+test_table = test_table.drop(columns=(0))
+test_table.index.name = 'ID'
+test_table = test_table.replace(r'^\s*$', np.nan, regex=True).dropna(axis=0,how='all').dropna(axis=1,how='all')
+test_table = test_table.replace(np.nan, 0, regex=True).replace('н', 0, regex=True)
+
+test_max_scores = test_table.iloc[0,len(test_table.iloc[0])-1]
+
+test_table = test_table.drop(index='ID').drop(columns=test_table.columns[len(test_table.columns)-1])
+
+def getVisitingByStudent():
+    vis = visiting_table.iloc[:, visiting_table.columns.get_level_values(1) == 'посещ-е'].sum(axis=1)
+    vis_count = sum(visiting_table.columns.get_level_values(1) == 'посещ-е')
     return vis, vis_count
 
-def getActivitygByStudent(index):
-    act = 0
-    for i in range(2,len(visiting_table.iloc[index]),2):
-        count = visiting_table.iloc[index,i]
-        if(not math.isnan(count)):
-            act += count
+def getActivitygByStudent():
+    act = visiting_table.iloc[:, visiting_table.columns.get_level_values(1) == 'актив'].sum(axis=1)
     return act
 
-def getHomeworkByStudent(index):
-    hw = 0
-    hw_count = 0
-    for i in range(1,len(homeworck_table.iloc[index])):
-        count = homeworck_table.iloc[index,i]
-        if(not isinstance(count,str) and not math.isnan(count)):
-            hw += count
-            hw_count += 1
+def getHomeworkByStudent():
+    hw = homework_table.sum(axis=1)
+    hw_count = len(homework_table.columns)
     return hw, hw_count
 
-def getTestByStudent(index):
-    index += 1
-    test = 0
-    test_count = 0
-    for i in range(1,len(test_table.iloc[index])):
-        count = test_table.iloc[index,i]
-        if(isinstance(count,str) and count != 'н'):
-            count = count.replace(',','.')
-            count = float(count)
-        if((not isinstance(count,str) and not math.isnan(count))):
-            test += count
-            test_count += test_table.iloc[0,i]
-        elif(count == 'н'):
-            test_count += test_table.iloc[0,i]
+def getTestByStudent():
+    test = test_table.sum(axis=1)
 
-    return test, test_count
+    return test
 
-IDs = []
-visiting = []
-activity = []
-pct_visiting = []
-pct_activity = []
-homeworck_scores = []
-test_scores = []
-results = []
-scores = []
+# def getScore():
+#     score = ''
 
-for i in range(len(visiting_table)):
-    id = visiting_table.iloc[i,0]
-    vis_count, max_vis_count = getVisitingByStudent(i)
-    act_count = getActivitygByStudent(i)
-    hw_score, max_hw_score = getHomeworkByStudent(i)
-    test_score, max_test_score = getTestByStudent(i)
+#     if(results[i] < 50):
+#         score = 'Н/А'
+#     elif(results[i] < 70):
+#         score = '3'
+#     elif(results[i] < 85):
+#         score = '4'
+#     else:
+#         score = '5'
 
-    IDs.append(id)
-    visiting.append(round(vis_count))
-    activity.append(round(act_count))
-    pct_visiting.append(round(vis_count/max_vis_count*100))
-    pct_activity.append(round(act_count/max_vis_count*100))
-    homeworck_scores.append(round(hw_score/6))
-    test_scores.append(round(test_score/max_test_score*100))
-    results.append(round(0.1 * (0.7*pct_visiting[i]+0.3*pct_activity[i])+0.2*homeworck_scores[i]+0.7*test_scores[i]))
+IDs = visiting_table.index
+visiting, max_vis_count = getVisitingByStudent()
+activity = round(getActivitygByStudent())
+pct_visiting = round(visiting / max_vis_count * 100)
+pct_activity = round(activity / max_vis_count * 100)
+homework_scores, homework_count = getHomeworkByStudent()
+homework_scores = round(homework_scores / homework_count)
+test_scores = round(getTestByStudent()/test_max_scores * 100)
+results = round(0.1 * (0.7*pct_visiting+0.3*pct_activity)+0.2*homework_scores+0.7*test_scores)
 
-    score = ''
+table = pd.DataFrame({'Посещение':visiting,'Активность':activity,'% посещения':pct_visiting,'% активности':pct_activity,'ДЗ':homework_scores,'КТ':test_scores,'Итог':results,'Оценка':''})
 
-    if(results[i] < 50):
-        score = 'Н/А'
-    elif(results[i] < 70):
-        score = '3'
-    elif(results[i] < 85):
-        score = '4'
-    else:
-        score = '5'
+table['Оценка'] = np.where(table['Итог'] < 50, 'Н/А',np.where(table['Итог'] < 70, '3',np.where(table['Итог'] < 85, '4','5')))
 
-    scores.append(score)
-
-table = pd.DataFrame({'ID':IDs,'Посещение':visiting,'Активность':activity,'% посещения':pct_visiting,'% активности':pct_activity,'ДЗ':homeworck_scores,'КТ':test_scores,'ИТОГ':results,'Оценка':scores})
-
-#print(table)
+print(table)
 
 table.to_parquet('lessons\\lab_1\\table.parquet',engine='fastparquet')
